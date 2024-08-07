47ba51dd363ba88deb0b0bedcbaf75d2eaf2ade7
ninafinageeva <finageeva.nina@mail.ru>
2024-08-07 22:10:40 +0300

    add apps study, users

---
+++ study/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+from users.models import User, NULLABLE
+
+
+class Students(models.Model):
+    first_name = models.CharField(max_length=100, **NULLABLE, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, **NULLABLE, verbose_name='Фамилия')
+    email = models.EmailField(max_length=250, **NULLABLE, unique=True, verbose_name='Адрес электронной почты')
+    comment = models.TextField(**NULLABLE, verbose_name='Комментарий')
+
+
+class Study(models.Model):
+    title = models.CharField(max_length=100, **NULLABLE, verbose_name='название')
+    link = models.URLField(**NULLABLE, verbose_name='ссылка на обучение')
+    plan = models.CharField(max_length=100, **NULLABLE, verbose_name='план обучения')
+    study_start = models.DateField(**NULLABLE, verbose_name='начало обучения')
+    student = models.ManyToManyField(Students, verbose_name='студент')
+    updated_at = models.DateTimeField(verbose_name='время обновления', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title} - {self.link}'
+
+    class Meta:
+        verbose_name = 'Обучение'
+        verbose_name_plural = 'Обучения'
+
+
+class Materials(models.Model):
+    title = models.CharField(max_length=100, verbose_name='название лекции')
+    content = models.TextField(**NULLABLE, verbose_name='содержание')
+    study_materials = models.ForeignKey(Study, **NULLABLE, on_delete=models.CASCADE, verbose_name='материалы обучения')
+    autor = models.CharField(max_length=255, **NULLABLE, verbose_name='автор контента')
+
+    def __str__(self):
+        return f'{self.title}: {self.content}'
+
+    class Meta:
+        verbose_name = 'материал'
+        verbose_name_plural = 'материалы'
+
---
+++ users/models.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class UserRoles(models.TextChoices):
+    owner = 'owner', gettext_lazy('owner')
+    admin = 'admin', gettext_lazy('admin')
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(max_length=250, unique=True, verbose_name='Адрес электронной почты')
+    first_name = models.CharField(max_length=100, **NULLABLE, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, **NULLABLE, verbose_name='Фамилия')
+    avatar = models.ImageField(upload_to='users/', **NULLABLE, verbose_name='Аватар')
+    comment = models.TextField(**NULLABLE, verbose_name='Описание')
+    role = models.CharField(max_length=20, **NULLABLE, choices=UserRoles.choices)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def str(self):
+        return f'{self.first_name} {self.last_name}: {self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
